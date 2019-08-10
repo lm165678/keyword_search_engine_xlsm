f7a4c23ea83303d5009be36cfc12991ac4f32e0c
Zhongjie Shen <zhongjis@uci.edu>
2019-08-09 21:35:15 -0700

    Modified InvertedIndexBuilder.java
    1: now calculation should be working properly
    2: add two print methods showing the content of two hashMaps - dict_fullNames_skills and dict_skill_fullNames
    3: change all the List and Map data types to ArrayList and HashMap
    4: some other minor syntax changes
    5: BUG: the fullNames list is never updated (line 42)

--- src/main/java/keyword_search_seedsprint/InvertedIndexBuilder.java
+++ src/main/java/keyword_search_seedsprint/InvertedIndexBuilder.java
@@ -1,8 +1,7 @@
 package keyword_search_seedsprint;
 
-import java.util.Map;
+import java.util.Map.*;
 import java.util.HashMap;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -11,48 +10,105 @@ import java.util.StringTokenizer;
  */
 class InvertedIndexBuilder {
 
-    Map<String, List> dict_fullname_skill; // dict[fullname] = [skill]
-    Map<String, List> dict_skill_fullname; // dict[skill] = [fullname]
+    HashMap<String, ArrayList<String>> dict_fullName_skills; // dict[fullName] = [skill]
+    HashMap<String, ArrayList<String>> dict_skill_fullNames; // dict[skill] = [fullName]
     TFIDFCalculator calculator;
 
     public InvertedIndexBuilder() {
-        dict_fullname_skill = new HashMap<String, List>();
-        dict_skill_fullname = new HashMap<String, List>();
+        dict_fullName_skills = new HashMap<String, ArrayList<String>>();
+        dict_skill_fullNames = new HashMap<String, ArrayList<String>>();
         this.calculator = new TFIDFCalculator();
         return;
     }
 
     public void add_token(String fullName, String allSkills) {
         StringTokenizer st = new StringTokenizer(allSkills, ", ");
+
+        ArrayList<String> skills = new ArrayList<String>();
+
         while (st.hasMoreTokens()) {
             String token = st.nextToken();
-            List fullNames = dict_fullname_skill.get(token);
+
+            // for dict_fullName_skills
+            skills.add(token);
+
+            // for dict_skill_fullNames
+            ArrayList<String> fullNames = this.dict_fullName_skills.get(token);
             if (fullNames == null) {
-                fullNames = new ArrayList();
+                fullNames = new ArrayList<String>();
                 fullNames.add(fullName);
+                this.dict_skill_fullNames.put(token, fullNames);
             } else {
+                // FIXME: the fullNames list is never updated
                 fullNames.add(fullName);
+                this.dict_skill_fullNames.replace(token, fullNames);
             }
-            dict_skill_fullname.put(token, fullNames);
-            System.out.println("Item Update {" + token + ": " + fullNames + "}");
+            // System.out.println("Item Update {" + token + ": " + fullNames + "}");
         }
+
+        dict_fullName_skills.put(fullName, skills);
         return;
     }
 
+    public void print_fullName_skill() {
+        for (Entry<String, ArrayList<String>> entry : this.dict_fullName_skills.entrySet()) {
+            System.out.println("fullName: " + entry.getKey());
+            System.out.println("skills: " + entry.getValue());
+        }
+    }
+
+    public void print_skill_fullName() {
+        for (Entry<String, ArrayList<String>> entry : this.dict_skill_fullNames.entrySet()) {
+            System.out.println("skill: " + entry.getKey());
+            System.out.println("names: " + entry.getValue());
+        }
+    }
+
     public void calculate() {
-        int doc_total;
-        int docs_total;
-        int term_occur_in_docs;
+        System.out.println("[SUCCESS] Start calculating...");
 
-        // for (String k : dict_fullname_skill.keySet()) {
-        //     List<String> fullnames = dict_fullname_skill.get(k);
-        //     term_occur_in_docs = fullNames.size();
-        //     doc_total = dict_fullname_skill
+        int doc_total; // total skill count under one entry
+        int docs_total = this.calculateDocsTotal(); // total skill count under all entries
+        int term_occur_in_docs; // total skill appearence under all entries
 
+        for (Entry<String, ArrayList<String>> entry : this.dict_skill_fullNames.entrySet()) {
+            String skill = entry.getKey();
+            ArrayList<String> names = entry.getValue();
 
-        //     double tfidf = calculator.tfIdf(, term_occur_in_docs);
-        // }
+            term_occur_in_docs = names.size();
+            System.out.println("[INFO] skill: " + skill + " term_occur_in_docs = " + term_occur_in_docs);
+
+            double tfidf = 0;
+            double totalTermCount = names.size();
+
+            for (String name : names) {
+                ArrayList<String> skills = this.dict_fullName_skills.get(name);
+                doc_total = skills.size();
+                double temp = calculator.tfIdf(doc_total, docs_total, term_occur_in_docs);
+                tfidf = tfidf + temp;
+            }
+            System.out.println(tfidf);
+            System.out.println(this.dict_skill_fullNames.get(skill));
+
+            tfidf = tfidf / totalTermCount;
+
+            System.out.println(skill + ": " + tfidf);
+
+        }
 
         return;
+    }
+    
+    private int calculateDocsTotal() {
+        int docs_total = 0;
+
+        for (Entry<String, ArrayList<String>> entry : this.dict_fullName_skills.entrySet()) {
+            for (String skill : entry.getValue())
+                docs_total++;
+        }
+
+        System.out.println("[INFO] docs_total = " + docs_total);
+        
+        return docs_total;
     }
 }
