4d4c698cc1f57d4bdb9374f1352b94ca8bd4eefa
Zhongjie Shen <zhongjis@uci.edu>
2019-12-04 15:42:45 -0800

    # [refactor] add runWithoutDB() to App for comparison
    # what I think:
    - looks like withoutDB(7s) is a lot faster compare to withDB(24s)

--- src/main/java/keyword_search_engine_xlsm/App.java
+++ src/main/java/keyword_search_engine_xlsm/App.java
@@ -49,13 +49,38 @@ public class App {
   }
 
   public static void runWithoutDB() {
-    return;
+    // extracting tokens
+    String folderDir = "src/main/resources/";
+    XlsmHandler handler = new XlsmHandler();
+    
+    handler.init(folderDir);
+    List<Pair<String, String>> pair_list = handler.extractWbs();
+
+    // adding tokens to indexBuilder
+    InvertedIndexBuilder indexBuilder = new InvertedIndexBuilder();
+
+    Iterator<Pair<String, String>> pairsIterator = pair_list.iterator();
+    Pair<String, String> p = pairsIterator.next(); // ignore the first title line
+
+    while (pairsIterator.hasNext()) {
+      p = pairsIterator.next();
+      indexBuilder.add_token(p.getKey(), p.getValue());
+    }
+
+    // caculating tdidfs
+    indexBuilder.calculate();
+
+    // print results
+    // indexBuilder.print_fullName_skill();
+    // indexBuilder.print_skill_fullName();
+    // indexBuilder.print_tfidfList();
   }
 
   public static void main(String[] args) {
     MessageHandler.successMessage("Program starting...");
 
     runWithDB();
+    // runWithoutDB();
 
     MessageHandler.successMessage("Program ending...");
   }
