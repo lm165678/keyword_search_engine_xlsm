c424ede0100ec8a81fb884b66b6e358ecac25b30
Zhongjie Shen <zhongjis@uci.edu>
2019-12-04 16:31:59 -0800

    # [feat/refactor] add stringInputMessage() to MessageHandler and remove some unnecessary syntax
    # Why:
    # now MessageHandler can handle user input in console.

--- src/main/java/keyword_search_engine_xlsm/util/MessageHandler.java
+++ src/main/java/keyword_search_engine_xlsm/util/MessageHandler.java
@@ -1,5 +1,6 @@
-
 package KeywordSearchEngine.util;
+
+import java.util.Scanner;
 
 /**
  * @author Zhongjie Shen
@@ -9,19 +10,23 @@ public class MessageHandler {
   public MessageHandler() {}
 
   public static String getSuccessMessage(String msg) {
-    return "[Success]" + " " + msg;
+    return "[Success] " + msg;
   }
 
   public static String getErrorMessage(String msg) {
-    return "[ERROR]" + " " + msg;
+    return "[ERROR] " + msg;
   }
 
   public static String getInfoMessage(String msg) {
-    return "[INFO]" + " " + msg;
+    return "[INFO] " + msg;
   }
 
   public static String getDebugMessage(String msg) {
-    return "[DEBUG]" + " " + msg;
+    return "[DEBUG] " + msg;
+  }
+
+  public static String getInputMessage(String msg) {
+    return "[NEED INPUT] " + msg;
   }
 
   public static void successMessage(String msg) {
@@ -42,5 +47,15 @@ public class MessageHandler {
   public static void debugMessage(String msg) {
     String outMsg = getDebugMessage(msg);
     System.out.println(outMsg);
+  }
+
+  public static String stringInputMessage(String msg) {
+    String outMsg = getInputMessage(msg);
+    Scanner sc = new Scanner(System.in);
+    System.out.println(outMsg);
+
+    String userInput = sc.nextLine();
+
+    return userInput;
   }
 }
